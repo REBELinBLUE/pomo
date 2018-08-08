5ad44322892e66ffc058eca82fdcd031d95b2406
Stephen Ball <stephen@rebelinblue.com>
2018-08-08 14:38:38 +0100

    Fix position of window on portrait screens

--- src/electron/mainWindow.js
+++ src/electron/mainWindow.js
@@ -1,5 +1,5 @@
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib/index'; // eslint-disable-line
-import { BrowserWindow } from 'electron'; // eslint-disable-line
+import { BrowserWindow, screen } from 'electron'; // eslint-disable-line
 import * as path from 'path';
 import { format as formatUrl } from 'url';
 import isDevelopment from './isDevelopment';
@@ -62,12 +62,19 @@ export default function createMainWindow() {
 export const positionWindowBelowTray = (window, tray) => {
   const windowBounds = window.getBounds();
   const trayBounds = tray.getBounds();
+  // const cursorPosition = screen.getCursorScreenPoint();
+  // FIXME: This is so that the tray position is correct on portrait screen.
+  //        This has been fixed in electron https://github.com/electron/electron/pull/13162
+  //        But for some reason still isn't working
+  const workArea = screen.getDisplayNearestPoint(screen.getCursorScreenPoint()).workArea
 
   // Center window horizontally below the tray icon
+  // const x = Math.round((trayBounds.x + (trayBounds.width / 2)) - (windowBounds.width / 2));
   const x = Math.round((trayBounds.x + (trayBounds.width / 2)) - (windowBounds.width / 2));
 
   // Position window 4 pixels vertically below the tray icon
-  const y = Math.round(trayBounds.y + trayBounds.height + 10);
+  // const y = Math.round(trayBounds.y + trayBounds.height + 10);
+  const y = Math.round(workArea.y + 10);
 
   window.setPosition(x, y, false);
 };
